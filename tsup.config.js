31cbbc2c666f83dcbedc087e9ded84dd1a240b38
Walter Buczacka <walterbuczacka@Walters-MacBook-Pro.local>
2024-11-05 14:50:51 -0300

    update tsup and add exports

--- tsup.config.js
+++ tsup.config.js
@@ -4,10 +4,7 @@ import {defineConfig} from "tsup"
 export default defineConfig(options => ({
   clean: !options.watch,
   entry: ["src/main.ts"],
-  format: ["esm"],
-  outExtension() {
-    return {js: ".js"}
-  },
+  format: ["esm", "cjs"],
   shims: true,
   sourcemap: !!options.watch,
   splitting: false,
