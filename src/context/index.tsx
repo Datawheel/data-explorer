c321dc63c0e55fa02254cc459dd18492753daa40
Walter Buczacka <walterbuczacka@Walters-MacBook-Pro.local>
2025-03-12 13:10:09 -0300

    update providers

--- src/context/index.tsx
+++ src/context/index.tsx
@@ -1,9 +1,14 @@
 import * as React from "react";
-import {QueryClient, QueryClientProvider, useQuery} from "@tanstack/react-query";
+import {QueryClient, QueryClientProvider} from "@tanstack/react-query";
 const queryClient = new QueryClient();
+import {TableRefreshProvider} from "../components/TableView";
 
 function AppProviders({children}) {
-  return <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>;
+  return (
+    <QueryClientProvider client={queryClient}>
+      <TableRefreshProvider>{children}</TableRefreshProvider>
+    </QueryClientProvider>
+  );
 }
 
 export {AppProviders};
