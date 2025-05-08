a059dfa18cd552a36edae906910fc92d15d116f8
Walter Buczacka <walterbuczacka@Walters-MacBook-Pro.local>
2025-05-08 12:12:35 -0300

    remove null values

--- src/vizbuilder/components/VizbuilderView.tsx
+++ src/vizbuilder/components/VizbuilderView.tsx
@@ -35,11 +35,14 @@ export function VizbuilderView(props: {cube: TesseractCube; params: QueryParams}
   const types = query.data?.types;
   const columns = Object.keys(types || {});
 
+  const filteredData = (data?.data || []).filter(row =>
+    Object.values(row).every(value => value !== null)
+  );
   const dataset = {
     columns: Object.fromEntries(
       columns.map(columnName => [columnName, buildColumn(cube, columnName, columns)])
     ),
-    data: data?.data || [],
+    data: filteredData,
     locale: params.locale || "en"
   };
 
